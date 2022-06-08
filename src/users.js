5c35c5d9e56566dc181fd58fd392620f2cb99f2f
RezEkoArd <rezkyardianto1222@gmail.com>
2022-06-09 00:47:11 +0700

    first-upload
    First-upload

---
+++ src/users.js
@@ -0,0 +1,143 @@
+export const Users = [
+    {
+        "id" : 1,
+        "first_name" : "Tono",
+        "last_name" : "Saprudin",
+        "email" : "Tono@gamil.com",
+        "gender" : "Male"
+    },
+    {
+        "id" : 2,
+        "first_name" : "Ardi",
+        "last_name" : "Sadil",
+        "email" : "ArdiSadil@gamil.com",
+        "gender" : "Male"
+    },
+    {
+        "id" : 3,
+        "first_name" : "Ardi",
+        "last_name" : "Sadil",
+        "email" : "ArdiSadil@gamil.com",
+        "gender" : "Male"
+    },
+    {
+        "id" : 4,
+        "first_name" : "Rezky",
+        "last_name" : "Pamungkas",
+        "email" : "Rezkypamungkas@gamil.com",
+        "gender" : "Male"
+    },
+    {
+        "id" : 5,
+        "first_name" : "Emilia",
+        "last_name" : "Flex",
+        "email" : "Emilia@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 6,
+        "first_name" : "Luna",
+        "last_name" : "Exlipe",
+        "email" : "LunaExlipe@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 7,
+        "first_name" : "Lina",
+        "last_name" : "Exlipe",
+        "email" : "LinaExlipe@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 8,
+        "first_name" : "Leon",
+        "last_name" : "Felipe",
+        "email" : "Leon123@gamil.com",
+        "gender" : "Male"
+    },
+    {
+        "id" : 9,
+        "first_name" : "Nico",
+        "last_name" : "Elfune",
+        "email" : "NicoElfune@gamil.com",
+        "gender" : "Male"
+    },
+    {
+        "id" : 10,
+        "first_name" : "Nico",
+        "last_name" : "Elfune",
+        "email" : "NicoElfune@gamil.com",
+        "gender" : "Male"
+    },
+    {
+        "id" : 11,
+        "first_name" : "Echinda",
+        "last_name" : "Maou",
+        "email" : "Echinda@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 12,
+        "first_name" : "Mirana",
+        "last_name" : "Moon",
+        "email" : "Miranadota@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 13,
+        "first_name" : "Yusa",
+        "last_name" : "Emi",
+        "email" : "YusaEmi@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 14,
+        "first_name" : "Ohana",
+        "last_name" : "Shijimi",
+        "email" : "Ohana@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 15,
+        "first_name" : "Sakura",
+        "last_name" : "Useless",
+        "email" : "SakuraUseless@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 16,
+        "first_name" : "Anya",
+        "last_name" : "Forger",
+        "email" : "AnyaForger@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 17,
+        "first_name" : "Loid",
+        "last_name" : "Forger",
+        "email" : "SpyxFamily@gamil.com",
+        "gender" : "Male"
+    },
+    {
+        "id" : 18,
+        "first_name" : "Yor",
+        "last_name" : "Forger",
+        "email" : "YorForger@gamil.com",
+        "gender" : "Female"
+    },
+    {
+        "id" : 19,
+        "first_name" : "Yuri",
+        "last_name" : "Briar",
+        "email" : "YuriBriar@gamil.com",
+        "gender" : "Male"
+    },
+    {
+        "id" : 20,
+        "first_name" : "Yamada",
+        "last_name" : "Hoshi",
+        "email" : "Yamada@gamil.com",
+        "gender" : "Male"
+    },
+
+]
